d2e7400d8005848bd7e3aa5116c41fd346c3da2d
Daniel Landolfi <d.landolfi@ufl.edu>
2024-02-23 19:06:59 -0500

    adding new favicon and editing sections descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
 	let sections = [
-		{ title: 'Projects', description: 'slkdslj kslk ' },
-		{ title: 'Quotes', description: 'sl djslfk sjkslk ' },
-		{ title: 'Stories', description: 'sf slkdf ;slk djslfk sjkslk ' }
+		{ title: 'Projects', description: 'Projects showcase' },
+		{ title: 'Quotes', description: 'Favorite quotes coming soon' },
+		{ title: 'Stories', description: 'Stories on the way' }
 	];
 </script>
 
@@ -10,7 +10,7 @@
 	<h2 class="text-2xl font-bold mx-4 mb-4">Things to look forward to</h2>
 
 	{#each sections as section (section.title)}
-		<a href="/" class="block card card-hover mx-4">
+		<a href="/" class="block card card-hover mx-4 my-4">
 			<header class="card-header text-xl">{section.title}</header>
 			<section class="p-4">{section.description}</section>
 		</a>
