ebfd614f0c2232aab6f423f89d0cf995ad4ad93c
dlandolfi <16345531+dlandolfi@users.noreply.github.com>
2024-02-24 14:41:40 -0500

    working on frontend for chat websocket

---
+++ src/lib/components/chat.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	let conn: WebSocket;
+	var msg = document.getElementById('msg');
+	var log = document.getElementById('log');
+
+	import { onMount } from 'svelte';
+
+	function appendLog(item: HTMLDivElement) {
+		if (log) {
+			const doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+			log.appendChild(item);
+			if (doScroll) {
+				log.scrollTop = log.scrollHeight - log.clientHeight;
+			}
+		}
+	}
+
+	const handleSubmit = () => {
+		if (!conn) {
+			return false;
+		}
+		if (!msg) {
+			return false;
+		}
+		conn.send(msg.innerHTML);
+		msg.innerHTML = '';
+		return false;
+	};
+
+	onMount(() => {
+		if (window.WebSocket) {
+			conn = new WebSocket(`ws://${document.location.host}/ws`);
+			conn.onclose = (evt) => {
+				const item = document.createElement('div');
+				item.innerHTML = '<b>Connection closed.</b>';
+				appendLog(item);
+			};
+			conn.onmessage = (evt) => {
+				const messages = evt.data.split('\n');
+				messages.forEach((message: string) => {
+					const item = document.createElement('div');
+					item.innerText = message;
+					appendLog(item);
+				});
+			};
+		} else {
+			const item = document.createElement('div');
+			item.innerHTML = '<b>Your browser does not support WebSockets.</b>';
+			appendLog(item);
+		}
+	});
+</script>
+
+<div id="log" class="m-8 p-8"></div>
+<form id="form" class="m-8" on:submit={handleSubmit}>
+	<input type="submit" value="Send" />
+	<input type="text" bind:value={msg} size="64" autofocus />
+</form>
+
+<style>
+	/* #log {
+		background: white;
+		margin: 0;
+		padding: 0.5em 0.5em 0.5em 0.5em;
+		position: absolute;
+		top: 0.5em;
+		left: 0.5em;
+		right: 0.5em;
+		bottom: 3em;
+		overflow: auto;
+	} */
+
+	/* #form {
+		padding: 0 0.5em 0 0.5em;
+		margin: 0;
+		position: absolute;
+		bottom: 1em;
+		left: 0px;
+		width: 100%;
+		overflow: hidden;
+	} */
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import Chat from '$lib/components/chat.svelte';
+</script>
+
 <div class="container mx-auto mt-8">
 	<h2 class="text-2xl font-bold mx-4 mb-4">Projects</h2>
+	<Chat />
 </div>
