ad47bc8f9d833ca9cbdd3f4993f80ea698232b84
Daniel Landolfi <d.landolfi@ufl.edu>
2024-02-24 00:09:53 -0500

    adding gandhi quote

--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,3 +1,9 @@
 <div class="container mx-auto mt-8">
 	<h2 class="text-2xl font-bold mx-4 mb-4">Quotes</h2>
+	<div class="card p-4 variant-soft">
+		<p>Nukes are not the answer.</p>
+		<p>They are the question.</p>
+		<p>And the answer is, "yes".</p>
+		<p><em>-Mahatma Gandhi</em></p>
+	</div>
 </div>
