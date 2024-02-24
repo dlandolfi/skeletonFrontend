0b2d26e6e611cb832e5d1d86e36ad5a6835d24be
Daniel Landolfi <d.landolfi@ufl.edu>
2024-02-24 00:16:10 -0500

    fixing quote margin

--- src/routes/quotes/+page.svelte
+++ src/routes/quotes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="container mx-auto mt-8">
 	<h2 class="text-2xl font-bold mx-4 mb-4">Quotes</h2>
-	<div class="card p-4 variant-soft">
+	<div class="card p-4 m-4 variant-soft">
 		<p>Nukes are not the answer.</p>
 		<p>They are the question.</p>
 		<p>And the answer is, "yes".</p>
