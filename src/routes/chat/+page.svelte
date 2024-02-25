b72384539f0ea6a57c7db7453c0a2dfcc4e12aa7
dlandolfi <16345531+dlandolfi@users.noreply.github.com>
2024-02-24 22:34:52 -0500

    renaming projects to chat

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="container mx-auto mt-8">
-	<h2 class="text-2xl font-bold mx-4 mb-4">Projects</h2>
+	<h2 class="text-2xl font-bold mx-4 mb-4">Chat Via WebSockets</h2>
 	<Chat />
 </div>
